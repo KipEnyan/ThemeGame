0f93e0c7bac74edf64e4e5a9ebc95a5c879f4197
Scott Todd <scoo531@gmail.com>
2013-10-29 23:09:46 -0400

    Spawn some randomized NPCs at the start
    Still using temp assets, just laying the groundwork
    Lacking:
    * predetermined/smart spawn locations (waiting on the environment to be setup)
    * preset tint colors (for skin tones)
    * named costume components (for conversations)
    
    addresses issue #2

---
+++ Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+        /*
+        if (hit.collider.gameObject.tag == "NPC") {
+            GameObject npc = hit.collider.gameObject;
+            NPCProperties npcProperties = (NPCProperties)npc.GetComponent("NPCProperties");
+
+            // Testing referencing variables from npcProperties in this script
+            Debug.Log("Collided, materialNumber: " + npcProperties.materialNumber +
+                      ", hatNumber: " + npcProperties.hatNumber);
+            Destroy(npc);
+        }
+        */
+    }
+}
