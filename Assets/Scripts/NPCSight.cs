42c999140d377efe36e08e6761467ea15fec3eb5
Nicholas Payne <nicholasfpayne@gmail.com>
2013-10-30 14:03:37 -0400

    NPC Sight framework. Part of issue #6.

---
+++ Assets/Scripts/NPCSight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCSight : MonoBehaviour {
+	
+	public float FOV = 90f;
+	public bool playerInSight;
+	public Vector3 lastKnownPosition;
+	
+	private NavMeshAgent nav;
+	private BoxCollider col;
+	private Animator anim;
+	private GameObject player;
+	
+	void Awake()
+	{
+		nav = GetComponent<NavMeshAgent>();
+		col = GetComponent<BoxCollider>();
+		anim = GetComponent<Animator>();
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
