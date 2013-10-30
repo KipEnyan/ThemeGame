dca70be844dd414f658bf373a588769521d8a429
Markus Gaasedelen <gaasem@rpi.edu>
2013-10-30 14:24:48 -0400

    Basic playerr actions, and speech bubble implemented

---
+++ Assets/playerScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+	
+	public float meleeDistance = 1;
+	
+	//Given a distance, check if there's an object
+	//infront of our player. Returns collider
+	RaycastHit raycastForward(float distance){
+		Vector3 fwdRay = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit = new RaycastHit();
+		
+		if(Physics.Raycast(transform.position, fwdRay, out hit, distance)){
+			Debug.DrawLine(transform.position, hit.point);
+		}
+
+		return hit;
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//process melee
+		if(Input.GetMouseButtonDown(0)){
+			Collider meleeVic = raycastForward(meleeDistance).collider;
+			
+			//do relevant collision stuff here
+			if(meleeVic){				
+				print("Hit " + meleeVic.name);
+				if(meleeVic.name == "dummy_man")
+					Destroy(meleeVic.gameObject);
+				
+			}
+		}
+		
+		
+		
+	}
+}
