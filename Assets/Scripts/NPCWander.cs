a197e6ebd7730565f3c405db96901f2d74050abe
Nicholas Payne <nicholasfpayne@gmail.com>
2013-11-01 15:34:27 -0400

    Basic wander implementation added. Addresses issue #5.

--- Assets/Scripts/NPCWander.cs
+++ Assets/Scripts/NPCWander.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NPCWander : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class NPCWander : MonoBehaviour 
+{
+	public bool isWandering = true;
+	public float xBound1 = 0f;
+	public float xBound2 = 50f;
+	public float zBound1 = 0f;
+	public float zBound2 = 50f;
+	private NavMeshAgent nav;
 	
+	void Awake()
+	{
+		nav = GetComponent<NavMeshAgent>();
+		nav.ResetPath();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+	{
+		if(isWandering && !nav.hasPath)
+		{
+			Vector3 dest = new Vector3(Random.Range(xBound1, xBound2), 0, Random.Range(zBound1, zBound2));
+			nav.SetDestination(dest);
+		}
 	}
 }
