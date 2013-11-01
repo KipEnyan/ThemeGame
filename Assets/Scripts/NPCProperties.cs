40a453b643f3417064cdcaa6ce3414c2771ff0ec
Nicholas Payne <nicholasfpayne@gmail.com>
2013-11-01 15:06:20 -0400

    Navmesh updated, NPC hearing added, NPC sight recognition of dead bodies added. Fixes issue #6.

--- Assets/Scripts/NPCProperties.cs
+++ Assets/Scripts/NPCProperties.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class NPCProperties : MonoBehaviour {
 
     // NPCSpawner sets these, other logic will use these to identify this NPC
+	public bool isDead = false;
     public int materialNumber;
     public int hatNumber;
     public Color tintColor;
--- Assets/Scripts/NPCSight.cs
+++ Assets/Scripts/NPCSight.cs
@@ -1,52 +1,97 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NPCSight : MonoBehaviour 
+public class NPCSight : MonoBehaviour
 {
 	public float FOV = 90f;
 	public bool playerInSight;
-	public Vector3 lastKnownPosition;
-	
+	public bool playerIsHeard;
+	public bool deadNPCInSight;
 	private NavMeshAgent nav;
 	private SphereCollider col;
-	private Animator anim;
 	private GameObject player;
 	
-	void Awake()
+	void Awake ()
 	{
-		//nav = GetComponent<NavMeshAgent>();
-		col = GetComponent<SphereCollider>();
-		//anim = GetComponent<Animator>();
-		player = GameObject.FindWithTag("Player");
+		nav = GetComponent<NavMeshAgent> ();
+		col = GetComponent<SphereCollider> ();
+		player = GameObject.FindWithTag ("Player");
 	}
-	
 	
 	void OnTriggerStay (Collider other)
 	{
-		if(other.gameObject == player)
-		{
+		if (other.gameObject == player) {
 			//print ("Player detected.");
 			playerInSight = false;
 			
 			Vector3 direction = other.transform.position - (transform.position + transform.up + transform.forward);
-			float angle = Vector3.Angle(direction, transform.forward);
+			float angle = Vector3.Angle (direction, transform.forward);
 			
-			if (angle < FOV * 0.5f)
-			{
+			if (angle < FOV * 0.5f) {
 				//print ("Player in FOV.");
 				RaycastHit hit;
-				Debug.DrawRay(transform.position + transform.up + transform.forward, direction);
+				//Debug.DrawRay(transform.position + transform.up + transform.forward, direction);
 				
-				if (Physics.Raycast(transform.position + transform.up + transform.forward, direction.normalized, out hit, col.radius))
-				{
+				if (Physics.Raycast (transform.position + transform.up + transform.forward, direction.normalized, out hit, col.radius)) {
 					//Debug.DrawLine(transform.position + transform.up + transform.forward, other.transform.position);
-					if (hit.collider.gameObject == player)
-					{
+					if (hit.collider.gameObject == player) {
 						//print ("Raycast hit player.");
 						playerInSight = true;
 					}
 				}
 			}
+			
+			if (CalculatePathLength (player.transform.position) <= col.radius) {
+				playerIsHeard = true;
+				//print ("I can hear you.");
+			} else {
+				playerIsHeard = false;	
+			}
+		} else if (other.gameObject.GetComponent<NPCProperties> () != null) {
+			if (other.gameObject.GetComponent<NPCProperties> ().isDead) {
+				deadNPCInSight = false;
+			
+				Vector3 direction = other.transform.position - (transform.position + transform.up + transform.forward);
+				float angle = Vector3.Angle (direction, transform.forward);
+			
+				if (angle < FOV * 0.5f) {
+					//print ("Dead NPC in FOV.");
+					RaycastHit hit;
+					//Debug.DrawRay(transform.position + transform.up + transform.forward, direction);
+				
+					if (Physics.Raycast (transform.position + transform.up + transform.forward, direction.normalized, out hit, col.radius)) {
+						//Debug.DrawLine(transform.position + transform.up + transform.forward, other.transform.position);
+						if (hit.collider.gameObject.GetComponent<NPCProperties> ().isDead) {
+							//print ("Raycast hit dead NPC.");
+							deadNPCInSight = true;
+						}
+					}
+				}
+			}
 		}
+		
+	}
+	
+	float CalculatePathLength (Vector3 targetPosition)
+	{
+		NavMeshPath path = new NavMeshPath ();
+		if (nav.enabled)
+			nav.CalculatePath (targetPosition, path);
+        
+		Vector3[] allWayPoints = new Vector3[path.corners.Length + 2];
+		allWayPoints [0] = transform.position;
+		allWayPoints [allWayPoints.Length - 1] = targetPosition;
+        
+		for (int i = 0; i < path.corners.Length; i++) {
+			allWayPoints [i + 1] = path.corners [i];
+		}
+        
+		float pathLength = 0;
+        
+		for (int i = 0; i < allWayPoints.Length - 1; i++) {
+			pathLength += Vector3.Distance (allWayPoints [i], allWayPoints [i + 1]);
+		}
+        
+		return pathLength;
 	}
 }
