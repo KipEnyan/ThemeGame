c5976bc30fffe1f1349fc49d82bed03f9fc38600
Scott Todd <scoo531@gmail.com>
2013-11-02 22:17:56 -0400

    Added a window to the ballroom
    Addresses issue #7

--- Assets/Scripts/TileMaterial.cs
+++ Assets/Scripts/TileMaterial.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 // Tile this object's _MainTex material proportionally based on its transform's localScale
+// set proportionalScale to manually set tiling numbers, independent of localScale
 public class TileMaterial : MonoBehaviour {
 
     public float xScaleMultipler = 1;
     public float yScaleMultipler = 1;
+    public bool proportionalScale = true;
 
 	void Start () {
-        float scaleX = transform.localScale.x * xScaleMultipler;
-        float scaleY = transform.localScale.z * yScaleMultipler;
+        float scaleX, scaleY;
+        if (proportionalScale) {
+            scaleX = transform.localScale.x * xScaleMultipler;
+            scaleY = transform.localScale.z * yScaleMultipler;
+        } else {
+            scaleX = xScaleMultipler;
+            scaleY = yScaleMultipler;
+        }
 
         renderer.material.SetTextureScale("_MainTex", new Vector2(scaleX, scaleY));
 	}
