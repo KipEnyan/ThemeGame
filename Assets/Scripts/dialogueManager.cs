5d06e304ce0529b0616a9e291d0459169662920a
Markus Gaasedelen <gaasem@rpi.edu>
2013-11-04 21:25:14 -0500

    Current dialogue system, pending NPC manager stuff for costume properties

---
+++ Assets/Scripts/dialogueManager.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dialogueManager : MonoBehaviour {
+	
+	private string[] greetings;
+	private string[] hints;
+	private string[] misc;
+	private string[] farewells;
+	private string[] panic;
+	private int line_len = 40;
+	//private NPCManager NPCMgr;
+	
+
+	void Start () {
+	
+		/* load dialogue from file and break up accordingly */
+		greetings = ((TextAsset)Resources.Load("greetings", typeof(TextAsset))).text.Split('\n');
+		hints = ((TextAsset)Resources.Load("hints", typeof(TextAsset))).text.Split('\n');
+		misc = ((TextAsset)Resources.Load("misc", typeof(TextAsset))).text.Split('\n');
+		farewells = ((TextAsset)Resources.Load("farewells", typeof(TextAsset))).text.Split('\n');
+		panic = ((TextAsset)Resources.Load("panic", typeof(TextAsset))).text.Split('\n');
+		
+		/* debug */
+		print(greetings.Length);
+		print("Done");
+	}
+	
+	
+	/* swap out npc properties in selected dialogue,
+	   break up dialogue into multiple lines, etc */
+	string processDialogue(string to_process, string speech_state){
+		string return_str = "";
+		
+		/* feature pending Scott's NPC manager / property access */
+		/*
+		NPC some_npc;
+		
+		if(speech_state == "hint")
+			some_npc = target;
+		else
+			some_npc = random_npc;
+		
+		to_process.Replace ("{mask}", some_npc.mask);
+		to_process.Replace ("{suit-color}", some_npc.suit);
+		to_process.Replace ("{tie-color}", some_npc.tie);
+		*/
+		
+		/* make sure our dialogue isn't one super huge line */
+		if(to_process.Length > line_len){
+			int linebreak_index = to_process.IndexOf (' ', line_len);
+			if(linebreak_index > 0){
+				char[] array = to_process.ToCharArray();
+				array[linebreak_index] = '\n';
+				return_str = new string(array);
+			}
+		}
+		
+		if(return_str.Length == 0)
+			return_str = to_process;
+		
+		return return_str;
+		
+	}
+	
+	
+	/* given a speech state, getDialogue will return relevant dialogue */
+	public string getDialogue(string speech_state){
+		string some_dialogue;
+		
+		/* select dialogue for corresponding conversation state */
+		switch(speech_state)
+		{
+			case "greeting":
+				some_dialogue = greetings[Random.Range (0, greetings.Length-1)]; break;
+			case "hint":
+				some_dialogue = hints[Random.Range (0, hints.Length-1)]; break;
+			case "misc":
+				some_dialogue = misc[Random.Range (0, misc.Length-1)]; break;
+			case "farewell":
+				some_dialogue = farewells[Random.Range (0, farewells.Length-1)]; break;
+			case "panic":
+				some_dialogue = panic[Random.Range (0, panic.Length-1)]; break;
+			default:
+				return "";
+		}
+		
+		/* finalize dialogue for display */
+		some_dialogue = processDialogue(some_dialogue, speech_state);
+		return some_dialogue;
+	}
+}
--- Assets/Scripts/speechScript.cs
+++ Assets/Scripts/speechScript.cs
@@ -4,31 +4,28 @@ using System.Collections;
 public class speechScript : MonoBehaviour {
 	
 	public Camera cameraToLookAt;
-	public float dialogSwitchTime = 3.0f;
-	private float dialogTimer;
-	
-	public string[] dialog = {"Hey! How are you this fine evening?",
-							"Scotch? I'm more of a whisky kinda guy."};
-	
+	private dialogueManager dialogueMgr;
+
 	// Use this for initialization
 	void Start () {
-		dialogTimer = dialogSwitchTime;
+		dialogueMgr = GameObject.Find("dialogueMgrObj").GetComponent<dialogueManager>();
 	}
-	
 	
 	void Update () {
 	
-		//Track speech billboard towards player in the XY direction
+		/* Track speech billboard towards player in the XY direction */
 		transform.LookAt( cameraToLookAt.transform.position );
 		transform.Rotate(0, 180, 0);
 		
-		//Switch dialog text after some amount of time
-		dialogTimer -= Time.deltaTime;
-		if(dialogTimer <= 0){
-			TextMesh temp = (TextMesh)gameObject.GetComponent(typeof(TextMesh));
-			temp.text = dialog[Random.Range(0, dialog.Length)];
-			dialogTimer = dialogSwitchTime;
-		}
+	}
+	
+	/* update speech text based on state */
+	public void updateSpeech(string player_state){
+		TextMesh temp = (TextMesh)gameObject.GetComponent(typeof(TextMesh));
 		
+		if(player_state == "")
+			temp.text = "";
+		else
+			temp.text = dialogueMgr.getDialogue(player_state);
 	}
 }
